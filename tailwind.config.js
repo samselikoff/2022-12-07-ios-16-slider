efa800b2768e13cac10f7c6c9ae03ad2076b67a9
Sam Selikoff <sam.selikoff@gmail.com>
2022-12-09 12:34:21 -0500

    End of demo

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.stone
+      }
+    },
+  },
+  plugins: [],
+}
